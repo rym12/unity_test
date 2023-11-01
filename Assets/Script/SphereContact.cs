38272a3a87e31394817c8c2c497e53b87f631d1b
rym12 <ryuma5401@gmail.com>
2023-11-01 17:34:06 +0900

    衝突量産バグ注意

--- Assets/Script/SphereContact.cs
+++ Assets/Script/SphereContact.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereDestroy : MonoBehaviour
+public class SphereContact : MonoBehaviour
 {
     public string type;
+    public GameObject next;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +25,16 @@ public class SphereDestroy : MonoBehaviour
 
         if (collision.gameObject.CompareTag(type)) //タグが"ball"のオブジェクトに接触した場合
         {
+            ChangeSphere();
             Destroy(this.gameObject); // このオブジェクトを破棄
         }
+    }
+
+    void  ChangeSphere()
+    {
+        // Sphereのインスタンスを生成
+        GameObject a = Instantiate(next, transform.position, Quaternion.identity);
+
+        // 生成したSphereを必要に応じて初期化するなどの処理を追加することができます
     }
 }
